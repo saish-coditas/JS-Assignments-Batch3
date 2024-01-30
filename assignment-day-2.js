b47385ed7b4f40bba871866bf01ddf638a460541
saish-coditas <158143241+saish-coditas@users.noreply.github.com>
2024-01-30 14:48:04 +0530

    Add files via upload

---
+++ assignment-day-2.js
@@ -0,0 +1,28 @@
+// Declare four variables without assigning values
+let customerName;
+let productPrice;
+let shippingAddress;
+let currentDate;
+
+// Declare four variables with assigned values
+const defaultLang='English';
+const maxAttempts=3;
+const isAdmin=false;
+const defaultColor='Blue';
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+const firstName = 'Saish';
+const lastName = 'Kothawade';
+const maritalStatus = 'Single';
+const country = 'India';
+const age = 22;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+const userFirstName='Saish', userLastName='Kothawade', userMaritalStatus='Single', userCountry='India', userAge=22;
+console.log(userCountry);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+const myAge = 22;
+const yourAge = 24;
+console.log(myAge);
+console.log(yourAge);
