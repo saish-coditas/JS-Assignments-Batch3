ec736446a9a16f01b14903067c20193c8c90d677
saish-coditas <158143241+saish-coditas@users.noreply.github.com>
2024-01-31 22:27:51 +0530

    Add files via upload

---
+++ assignment-day-3.js
@@ -0,0 +1,21 @@
+/*
+Assignment Day 3
+Create a file and declare variables and assign string, boolean, undefined and null data types
+Create file and use the JavaScript typeof operator to check different data types. Check the data type of each variable.
+*/
+
+const firstName = 'Saish';
+const isAdmin = true;
+const studentAge = 22;
+const cars = ['Audi', 'BMW', 'Hyundai', 'Kia'];
+let username;
+const userPermission = null;
+const worldGDP = 112345678091234567890123456780123456780n;
+
+console.log(typeof firstName);
+console.log(typeof isAdmin);
+console.log(typeof studentAge);
+console.log(typeof username);
+console.log(typeof userPermission);
+console.log(typeof cars);
+console.log(typeof worldGDP);
